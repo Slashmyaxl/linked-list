1083575e8d136c3d0ab0e135408b93f679d6b2b1
Slashmyaxl <140274090+Slashmyaxl@users.noreply.github.com>
2024-03-04 09:34:03 -0500

    Initial commit

--- src/hashmap.js
+++ src/hashmap.js
@@ -1,40 +1,75 @@
-import nodeMaker from "./nodeMaker.js";
+import linkedList from "./linkedlist.js";
 
 export default function hashMap (size) {
-    let buckets = new Array(size)
+    let buckets = new Array(size);
 
-    buckets[5] = nodeMaker('Samson');
-
-    return {
-        buckets,
-        hash: function(key) {
-            let hashCode = 0;
-            const primeNumber = 31;
-            for (let i = 0; i < key.length; i++) {
+    const hash = (key) => {
+        let hashCode = 0;
+        const primeNumber = 31;
+        for (let i = 0; i < key.length; i++) {
             hashCode = (primeNumber * hashCode + key.charCodeAt(i)) % size;
             }
 
-            return hashCode
-        },
-        set: function(key, value) {
-            if (buckets[key] && buckets[key].value === value) buckets[key].value = value
-            else if (buckets[key]) buckets[key].next = nodeMaker(value);
-            else buckets[key] = nodeMaker(value);
-        }
-        get: function(key) {
+        return hashCode
+    }
 
+    return {
+        buckets,
+        set: function(key, value) {
+            const index = hash(key);
+            if (!buckets[index]) buckets[index] = linkedList([key, value]);
+            else if (buckets[index].contains(key)) {
+                const linkedIndex = buckets[index].find(key);
+                buckets[index].at(linkedIndex).value = [key, value];
+            }
+            else buckets[index].append([key, value]);
+        },
+        get: function(key) {
+            const index = hash(key);
+            if (buckets[index].contains(key)) {
+                const keyIndex = buckets[index].find(key);
+                return buckets[index].at(keyIndex).value[1];
+            }
+            return null
+        },
+        has: function (key) {
+            const index = hash(key);
+            if (buckets[index].contains(key)) return true
+            return false
+        },
+        remove: function (key) {
+            const index = hash(key);
+            if (buckets[index].contains(key)) {
+                const linkedIndex = buckets[index].find(key);
+                buckets[index].removeAt(linkedIndex);
+                return true
+            }
+            return false
         }
     }
 }
 
 const myBuckets = hashMap(15);
 
-console.log(myBuckets);
-console.log(myBuckets.hash('Danny'))
-console.log(myBuckets.hash('Manon'))
-console.log(myBuckets.hash('Suzie'))
 
-myBuckets.set(myBuckets.hash('Danny'), 'Danny')
-myBuckets.set(myBuckets.hash('Manon'), 'Manon')
-myBuckets.set(myBuckets.hash('Manon'), 'Manon')
-console.log(myBuckets.buckets)
+myBuckets.set('Suzi', 'Par');
+myBuckets.set('Dara', 'Lara');
+myBuckets.set('Suzi', 'Lara');
+myBuckets.set('Terry', 'Brad');
+myBuckets.set('Joe', 'Gren');
+myBuckets.set('Locke', 'Lamora');
+
+myBuckets.set('Father', 'Chains');
+myBuckets.set('Lynn', 'Swann');
+myBuckets.set('Cake', 'helps others');
+myBuckets.set('Toby', 'plays instruments');
+myBuckets.set('Carlos', 'has an office');
+
+myBuckets.set('Cake', 'is delicious');
+
+
+console.log(myBuckets.get('Locke'));
+console.log(myBuckets.remove('Locke'));
+console.log(myBuckets.buckets[15])
+console.log(myBuckets.has('Locke'));
+
